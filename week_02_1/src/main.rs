1c0a29a0280db8b08901b38d06de206a7c857ecc
Stuart Haidon <serayen.sh@gmail.com>
2017-06-27 19:10:16 +0100

    Added rendering of the @ symbol.

--- week_02_1/src/main.rs
+++ week_02_1/src/main.rs
@@ -1,5 +1,7 @@
 extern crate tcod;
 use tcod::RootConsole;
+use tcod::{Console};
+use tcod::console::{FontLayout, FontType, BackgroundFlag};
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HIGHT: i32 = 50;
@@ -9,10 +11,16 @@ fn main() {
                     .size(SCREEN_WIDTH, SCREEN_HIGHT)
                     .title("Roguelike Tutorial")
                     .fullscreen(false)
+                    .font("arial10x10.png", FontLayout::Tcod)
+                    .font_type(FontType::Greyscale)
                     .init();
 
+    root.set_default_foreground(tcod::colors::WHITE);
+
     while !root.window_closed() {
+        root.put_char(1, 1, '@', BackgroundFlag::None);
         root.flush();
+
         let key = root.wait_for_keypress(true);
     }
 }
